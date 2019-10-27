33ad654b1e2c9f42960cb34eddf6a5ee0a4a8b8e
Minh Quan <18120520@student.hcmus.edu.vn>
2019-10-27 11:25:50 +0700

    Add EPI Function

--- 18120506_18120520/Bool.cpp
+++ 18120506_18120520/Bool.cpp
@@ -205,3 +205,168 @@ vector<string> markBit(vector<vector<string>> group) {
 
 	return result;
 }
+
+//Filter ra cac EPI con lai
+void epiFilterRest(int table[][50], int start, vector<string> pi, vector<int> minterm,
+	unordered_set<int> decSet, vector<vector<string>>& listEPI, vector<string> bin) {
+	bool flag = false;
+	for (int i = 0; i < minterm.size(); i++) {
+		if (decSet.find(minterm[i]) == decSet.end()) {
+			flag = true;
+			break;
+		}
+	}
+
+	if (!flag) {
+		listEPI.push_back(bin);
+		return;
+	}
+
+	for (int i = start; i < pi.size(); i++) {
+		if (pi[i] != "none") {
+			unordered_set<int> tempSet = decSet;
+			vector<string> tempBin = bin;
+			bool isEPI = false;
+
+			for (int j = 0; j < pow(2, n); j++) {
+				if (table[i][j] == 1 && decSet.find(j) == decSet.end()) {
+					isEPI = true;
+					tempSet.insert(j);
+				}
+			}
+
+			if (isEPI) {
+				tempBin.push_back(pi[i]);
+				epiFilterRest(table, start + 1, pi, minterm, tempSet, listEPI, tempBin);
+			};
+		}
+	}
+
+}
+
+//Filter ra nhung EPI tu PI
+//Input: list cac PI, list cac minterm
+//output: list cac EPI
+vector<vector<string>> epiFilter(vector<string> pi, vector<int> minterm) {
+	int table[50][50];
+	unordered_set<int> decSet(0);
+	vector<string> tempBin;
+	vector<vector<string>> listEPI;
+	int tempDec;
+
+	for (int i = 0; i < pi.size(); i++) {
+		for (int j = 0; j < pow(2, n); j++) {
+			table[i][j] = 0;
+		}
+	}
+
+	for (int i = 0; i < pi.size(); i++) {
+		tempBin = binGenerator(pi[i]);
+
+		for (int j = 0; j < tempBin.size(); j++) {
+			tempDec = binToDec(tempBin[j]);
+			table[i][tempDec] = 1;
+		}
+	}
+
+	tempBin.resize(0);
+	for (int i = 0; i < pi.size(); i++) {
+		for (int j = 0; j < pow(2, n); j++) {
+			if (table[i][j] == 1) {
+				int sum = 1;
+				for (int k = i + 1; k < pi.size() + i; k++) {
+					sum += table[k % pi.size()][j];
+				}
+
+				if (sum == 1) {
+					for (int h = 0; h < pow(2, n); h++) {
+						if (table[i][h] == 1) {
+							if (decSet.find(h) == decSet.end()) {
+								decSet.insert(h);
+							}
+						}
+					}
+
+					tempBin.push_back(pi[i]);
+					pi[i] = "none";
+					break;
+				}
+			}
+		}
+	}
+
+	epiFilterRest(table, 0, pi, minterm, decSet, listEPI, tempBin);
+
+	//Chon nhung cong thuc co so don thuc nho nhat
+	int min = listEPI[0].size();
+	for (int i = 1; i < listEPI.size(); i++) {
+		if (min > listEPI[i].size()) {
+			min = listEPI[i].size();
+		}
+	}
+
+	for (int i = 0; i < listEPI.size(); i++) {
+		if (min != listEPI[i].size()) {
+			listEPI.erase(listEPI.begin() + i);
+		}
+	}
+
+	return listEPI;
+}
+
+// Chuyen nhung EPI tu dang bin sang normal term
+//Input list pi dang Bin, output: string normal term
+string toEPIString(vector<string> pi) {
+	string str = "";
+
+	for (int i = 0; i < pi.size(); i++) {
+		for (int j = 0; j < pi[i].length(); j++) {
+			if (pi[i][j] == '0') {
+				str = str + token[j] + '\'';
+			}
+			else if (pi[i][j] == '1') {
+				str += token[j];
+			}
+			else continue;
+		}
+
+		if (i != pi.size() - 1) str += " + ";
+	}
+
+	return str;
+}
+
+//Lay do dai cua cong thuc da thuc toi tieu dang normal term (ko bao gom '+' hoac ' ' hoac ')
+int getEPIStringLength(string epi) {
+	int length = 0;
+	for (int i = 0; i < epi.length(); i++) {
+		if (epi[i] != '\'' && epi[i] != ' ' && epi[i] != '+') {
+			length++;
+		}
+	}
+	return length;
+}
+
+//Loc ra nhung cong thuc da thuc toi tieu co do dai EPI String nho nhat
+vector<string> minEPIString(vector<vector<string>> listEPI) {
+	vector<string> listEPIString(0);
+
+	for (int i = 0; i < listEPI.size(); i++) {
+		listEPIString.push_back(toEPIString(listEPI[i]));
+	}
+
+	int min = getEPIStringLength(listEPIString[0]);
+	for (int i = 1; i < listEPIString.size(); i++) {
+		if (min > getEPIStringLength(listEPIString[i])) {
+			min = getEPIStringLength(listEPIString[i]);
+		}
+	}
+
+	for (int i = 0; i < listEPIString.size(); i++) {
+		if (min != getEPIStringLength(listEPIString[i])) {
+			listEPIString.erase(listEPIString.begin() + i);
+		}
+	}
+
+	return listEPIString;
+}
